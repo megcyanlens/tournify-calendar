22c8e704adad2c946334bb7e8257aca516376064
megcyanlens <meg@cyanlens.com>
2026-06-01 14:19:24 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+document
+  .getElementById('generate')
+  .addEventListener('click', () => {
+
+    const url =
+      document.getElementById('teamUrl').value;
+
+    console.log('Generate calendar for:', url);
+
+  });
