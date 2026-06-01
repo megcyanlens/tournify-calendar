447909d56e7456e2372f548713ec83f75d4bb12e
megcyanlens <meg@cyanlens.com>
2026-06-01 14:29:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 document
   .getElementById('generate')
-  .addEventListener('click', () => {
+  .addEventListener('click', async () => {
 
     const url =
       document.getElementById('teamUrl').value;
 
-    console.log('Generate calendar for:', url);
+    try {
+
+      const response =
+        await fetch(url);
+
+      const html =
+        await response.text();
+
+      console.log(html.substring(0, 1000));
+
+    } catch (error) {
+
+      console.error(error);
+
+    }
 
   });
