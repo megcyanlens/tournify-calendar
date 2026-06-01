a43fa40584504213a00b49f164984c0c07f3a16c
megcyanlens <meg@cyanlens.com>
2026-06-01 14:48:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-document
-  .getElementById('generateBtn')
-  .addEventListener('click', async () => {
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
-    const url =
-      document.getElementById('teamUrl').value;
+const firebaseConfig = {
+  apiKey: "AIzaSyDpqIP2y0ZBWjAcknp1szptkyh0fk6zGQI",
+  authDomain: "tournamentsoftware-a1b3d.firebaseapp.com",
+  projectId: "tournamentsoftware-a1b3d",
+  storageBucket: "tournamentsoftware-a1b3d.appspot.com",
+  messagingSenderId: "659831913509",
+  appId: "1:659831913509:web:1295743f7e5becfcaf13cc"
+};
 
-    const parts =
-      url.split('/');
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-    const liveLink =
-      parts[4];
+window.testFirestore = async () => {
 
-    const teamId =
-      parts[6];
+  const snapshot =
+    await getDocs(collection(db, 'tournaments'));
 
-    console.log({
-      liveLink,
-      teamId
-    });
+  console.log(
+    snapshot.docs.length
+  );
 
-});
+};
