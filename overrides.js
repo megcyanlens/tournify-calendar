1320f028d8c6cf81d1ae0f50b0460ff29d8b8503
megcyanlens <meg@cyanlens.com>
2026-06-03 17:53:06 +0100

    Create overrides.js

---
+++ overrides.js
@@ -0,0 +1,13 @@
+window.TOURNAMENT_OVERRIDES = {
+
+  bigbowl2026: {
+
+    location:
+      'Turngesellschaft Walldorf 1896 e.V., Okrifteler Straße, Mörfelden-Walldorf, Deutschland',
+
+    mapsUrl:
+      'https://www.google.com/maps/place/Turngesellschaft+Walldorf+E.+V./@50.0038757,8.5648317,17z/data=!3m1!4b1!4m6!3m5!1s0x47bd75108abc9c5b:0xad4afa30130ff509!8m2!3d50.0038723!4d8.567412!16s%2Fg%2F1tfxx493?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D'
+
+  }
+
+};
